aab9763bc05d88a03e78439134d90e9f44c9295b
seprjpaha <patrik.hallner@ascom.com>
2018-09-19 17:00:15 +0200

    style products

---
+++ Ruby/Ruby/Scripts/products.js
@@ -0,0 +1,23 @@
+﻿var Products = {};
+
+(function (Products) {
+    'use strict';
+
+    Products.initPage = function () {
+        $(document).on('click', '#JobWithDestination', Products.showLocations);
+
+        var isMobile = window.matchMedia('only screen and (max-width: 760px)');
+
+        if (!isMobile.matches) {
+            $('.products').addClass('in');
+        };
+    };
+    Products.showLocations = function () {
+        $('.AACMultiselectBox [name*="JobTypeLocation"]').parents('li').toggle();
+    };
+
+    $(document).ready(function () {
+        Products.initPage();
+    });
+
+}(Products));
